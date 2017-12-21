0e89d90b7b379d30b2e300b129db2bdecc852f77
unknown <haci@hacikho.com>
2017-12-21 13:22:36 -0500

    login logut is working

---
+++ FurnitureSale/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FurnitureSale.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult AdminLogin()
+        {
+            return View("AdminLogin");
+        }
+
+        [HttpPost]
+        public ActionResult AdminLogin(FormCollection frmc)
+        {
+            /// Extracting the value from FormCollection
+            string name = frmc["userName"];
+            string pwd = frmc["Password"];
+            if(name=="Admin" && pwd ==  "password")
+            {
+                Session["UserName"] = "Admin";
+                return View("NewProduct");
+            }
+            else
+            {
+                return View("AdminLogin");
+            }
+
+        }
+
+        public ActionResult logout()
+        {
+            Session.Abandon();
+            return View("Logout");
+            
+        }
+
+    }
+}
+
--- FurnitureSale/Global.asax.cs
+++ FurnitureSale/Global.asax.cs
@@ -19,6 +19,15 @@ namespace FurnitureSale
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+
+
+        protected void Application_BeginRequest()
+        {
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Cache.SetExpires(DateTime.UtcNow.AddHours(-1));
+            Response.Cache.SetNoStore();
+        }
+
         protected override IKernel CreateKernel()
         {
             var kernel = new StandardKernel();
